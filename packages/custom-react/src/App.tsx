d76fc0923cb819e100f7a33a929a7011cf4816fd
Sankar boro <sankar.boro@yahoo.com>
2022-01-12 20:42:08 +0530

    building from root

--- packages/custom-react/src/App.tsx
+++ packages/custom-react/src/App.tsx
@@ -3,5 +3,5 @@ import { Hello } from "lily-components/src/index";
 
 export default function App() {
   console.log(Hello())
-  return <strong>Hello World!. Custom React App.</strong>;
+  return <strong>Hello World!. Custom React App. Test hot Reload.</strong>;
 }
---
+++ scripts/dev-dependencies-install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+cd ..
+rm -rf ./node_modules
+yarn add react react-dom
+
+yarn add -D typescript \
+@types/react \
+@types/react-dom \
+@babel/core \
+@babel/preset-env \
+@babel/preset-react \
+@babel/preset-typescript \
+@babel/plugin-transform-runtime \
+@babel/runtime \
+eslint \
+eslint-plugin-react \
+eslint-plugin-react-hooks \
+@typescript-eslint/parser \
+@typescript-eslint/eslint-plugin \
+webpack \
+webpack-cli \
+@types/webpack \
+webpack-dev-server \
+@types/webpack-dev-server \
+babel-loader \
+ts-node \
+fork-ts-checker-webpack-plugin \
+@types/fork-ts-checker-webpack-plugin
+
+
+yarn add -D eslint \
+eslint-plugin-react \
+eslint-plugin-react-hooks
