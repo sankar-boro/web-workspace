4018d91d7150824861141a4300f6cfe658a83624
Sankar boro <sankar.boro@yahoo.com>
2022-01-13 09:42:53 +0530

    tests passed lily-workspace

--- packages/custom-react/src/App.tsx
+++ packages/custom-react/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Hello, SomeData } from "lily-components";
 
 export default function App() {
+  console.log(Hello());
   const data = SomeData().val;
 
   return <>{data}</>;
